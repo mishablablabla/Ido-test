2a41dbcae1498f88120fd5cfc4dea13bb458527b
Миша Зуев <mzuew2006@gmail.com>
2025-01-27 00:17:04 +0100

    2nd try :)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", updateActiveLink);
+
   const btn = document.querySelectorAll(".buy__btn"),
     modal = document.querySelector(".modal"),
     modalClose = document.querySelector(".modal__close"),
     listItems = document.querySelector(".grid"),
-    dbUrl = "https://brandstestowy.smallhost.pl/api/random",
+    dbUrl =
+      "https://brandstestowy.smallhost.pl/api/random?pageNumber=3&pageSize=50", // w mailu był link do api  https://brandstestowy.smallhost.pl/api/random ale tam są tylko 20 itemów, w api który jest wykorzystany są 50 itemów
     modalText = document.querySelector(".modal__text-name"),
     modalId = document.querySelector(".modal__text-id"),
     modalToBuy = document.querySelector(".modal-form__to__buy"),
@@ -69,8 +72,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
       controlModal(modal, "show", "hide", "hidden", dataText, dataId);
       console.log(event.target.dataset);
-
-      loadTask();
     }
   });
 
@@ -92,23 +93,95 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  async function loadTask() {
+  async function loadTask(limit) {
+    const productsMessage = document.querySelector(".products__message");
+
+    controlModal(productsMessage, "show", "hide");
+
     listItems.innerHTML = "";
 
     try {
       const data = await getData(dbUrl);
 
-      generateItems(data.data);
+      generateItems(data.data, limit);
     } catch (error) {
       console.error(error);
+    } finally {
+      controlModal(productsMessage, "hide", "show");
     }
   }
 
-  function generateItems(arr) {
-    arr.forEach((item) => {
-      const newItem = new Cards(item.id, item.text).render();
+  function generateItems(arr, limit = 5) {
+    const showArr = arr.slice(0, limit);
+
+    showArr.forEach((item) => {
+      new Cards(item.id, item.text).render();
     });
   }
+
+  // show current section in header
+
+  const sections = document.querySelectorAll("section"),
+    navLinks = document.querySelectorAll(".nav__link");
+
+  function updateActiveLink() {
+    let currentSection = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop,
+        sectionHeight = section.offsetHeight;
+
+      if (
+        window.scrollY >= sectionTop - window.innerHeight / 2 &&
+        window.scrollY < sectionTop + sectionHeight - window.innerHeight / 2
+      ) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").includes(currentSection)) {
+        link.classList.add("active");
+      }
+    });
+  }
+
+  // current of items
+
+  const currentItem = document.querySelector("#products-count");
+
+  currentItem.addEventListener("change", () => {
+    const currentItemValue = currentItem.value;
+    console.log(currentItemValue);
+    loadTask(currentItemValue);
+  });
+
+  // burger menu
+
+  const burgerButton = document.querySelector(".burger-button"),
+    burgerNav = document.querySelector(".burger-nav"),
+    closeButton = document.querySelector(".close__button"),
+    burgerItem = document.querySelectorAll(".burger__menu-item");
+
+  burgerButton.addEventListener("click", () => {
+    burgerNav.classList.toggle("open");
+    document.body.style.overflow = `hidden`;
+    document.body.style.background = "#fff";
+  });
+
+  closeButton.addEventListener("click", () => {
+    burgerNav.classList.toggle("open");
+    document.body.style.overflow = ``;
+  });
+
+  burgerItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      burgerNav.classList.toggle("open");
+      document.body.style.overflow = `hidden`;
+      document.body.style.background = "#fff";
+    });
+  });
 });
 
 // Modal to buy -  w razie potrzeby mogę dodać taką funkcjonalność, malutkie ządanie POST do serwera(localStorage albo JSON server) i wszystko :)
